df40270a58e3c8d9f1daad5954f563df59f15669
SHAW101298 <shaw101298@gmail.com>
2023-01-08 18:14:52 +0100

    Poprawienie wartosci

--- Assets/Scripts/Player/Abilities/Player_Paladin_AroundAttack.cs
+++ Assets/Scripts/Player/Abilities/Player_Paladin_AroundAttack.cs
@@ -8,8 +8,8 @@ public class Player_Paladin_AroundAttack : Ability_2
     public GameObject weaponHolder;
 
     public float rechargeTime = 5;
-    public float rotationTime = 1;
-    public Vector3 rotationSpeed = new Vector3(0,0,5);
+    public float rotationTime = 0.26f;
+    public Vector3 rotationSpeed = new Vector3(0,0,-1500);
     public Vector3 weaponScale = new Vector3(1.5f,1,1);
 
     public float energyCost = 15;
@@ -24,6 +24,7 @@ public class Player_Paladin_AroundAttack : Ability_2
             player.stats.ModifyEnergy(-energyCost);
             isUsed = true;
             timer = 0;
+            player.controller.weaponCollider.enabled = true;
         }
     }
     private void Update()
@@ -41,6 +42,7 @@ public class Player_Paladin_AroundAttack : Ability_2
                 isRecharching = true;
                 isUsed = false;
                 player.canRotateWeapon = true;
+                player.controller.weaponCollider.enabled = false;
                 weaponHolder.transform.rotation.eulerAngles.Set(0, 0, 0);
                 weaponHolder.transform.localScale = Vector3.one;
             }
