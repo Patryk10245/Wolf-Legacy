7943270c20b673cfae1e8e7a64733c54aba4ef4b
SHAW101298 <shaw101298@gmail.com>
2023-01-08 17:59:36 +0100

    Update Stop_Rotation.cs

--- Assets/Scripts/Stop_Rotation.cs
+++ Assets/Scripts/Stop_Rotation.cs
@@ -5,27 +5,12 @@ using UnityEngine.AI;
 
 public class Stop_Rotation : MonoBehaviour
 {
-    public Transform gracz;
     public NavMeshAgent agent;
-    public bool follow;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-    }
-
-        
-    void Update()
-    {
-        if(follow == true)
-        {
-            agent.SetDestination(gracz.position);
-        }
-        if(Input.GetKeyDown("space"))
-        {
-            follow = !follow;
-        }
     }
 }
