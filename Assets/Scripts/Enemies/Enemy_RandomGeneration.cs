34dcc64150f45601e28ef61e648446aabb469c4e
SHAW101298 <shaw101298@gmail.com>
2023-01-16 16:43:11 +0100

    Poprawienie tworzenia

--- Assets/Scripts/Enemies/Enemy_RandomGeneration.cs
+++ Assets/Scripts/Enemies/Enemy_RandomGeneration.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_RandomGeneration : MonoBehaviour
 {
@@ -10,19 +11,23 @@ public class Enemy_RandomGeneration : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SpawnEnemies();
+        }
     }
 
     void SpawnEnemies()
     {
         int random = Random.Range(0, maxNumberOfSpawnedEnemies);
-
+        GameObject temp;
         Debug.Log("Enemies spawned = " + random);
 
         for(int i  = 0; i < random; i++)
         {
             Vector3 randomPos = new Vector3(Random.Range(corners[0].position.x, corners[1].position.x), Random.Range(corners[0].position.y, corners[1].position.y), transform.position.z);
-            Instantiate(enemyToSpawn, randomPos, transform.rotation);
+            temp = Instantiate(enemyToSpawn, randomPos, transform.rotation);
+            temp.GetComponent<NavMeshAgent>().SetDestination(temp.transform.position + Vector3.left);
         }
         Destroy(gameObject);
     }
