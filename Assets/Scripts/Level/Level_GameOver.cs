400dc31504aa78e924032dacf23638f6a799a31c
SHAW101298 <shaw101298@gmail.com>
2023-01-18 19:05:05 +0100

    Cofniecie do wioski z pauzy

--- Assets/Scripts/Level/Level_GameOver.cs
+++ Assets/Scripts/Level/Level_GameOver.cs
@@ -11,20 +11,23 @@ public class Level_GameOver : MonoBehaviour
         Game_State.gamePaused = true;
         Time.timeScale = 0;
         Game_CanvasController.ins.DeathScreen();
-        ScoreTable.ins.ReduceCollectedGoldByDeath();
         //ScoreTable.ins.ApplyCollectedGold();
-        GameSetup.ins.SaveClassData();
+        
 
     }
 
     public void UI_ReturnToVillage()
     {
+        ScoreTable.ins.ReduceCollectedGoldByDeath();
         ScoreTable.ins.ApplyCollectedGold();
+        GameSetup.ins.SaveClassData();
         Level_SelectedScenes.ins.ChangeToVillageScene();
     }
     public void UI_ReturnToMenu()
     {
+        ScoreTable.ins.ReduceCollectedGoldByDeath();
         ScoreTable.ins.ApplyCollectedGold();
+        GameSetup.ins.SaveClassData();
         Level_SelectedScenes.ins.ChangeToMainmenu();
     }
 }
