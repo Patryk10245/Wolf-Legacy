1347a1c0594627b30b306894dfb25675a6e8decf
SHAW101298 <shaw101298@gmail.com>
2023-01-19 21:35:36 +0100

    Update Destroyable_Object.cs

--- Assets/Scripts/Destroyable_Object.cs
+++ Assets/Scripts/Destroyable_Object.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Destroyable_Object : MonoBehaviour
 {
     public GameObject objectToSpawnOnDeath;
+    public GameObject particle;
     public void DestroyMe()
     {
         Debug.LogWarning("Play Particle");
         if(objectToSpawnOnDeath != null)
         {
             Instantiate(objectToSpawnOnDeath, gameObject.transform.position, gameObject.transform.rotation);
+            Instantiate(particle, gameObject.transform.position, gameObject.transform.rotation);
         }
         Destroy(gameObject);
     }
