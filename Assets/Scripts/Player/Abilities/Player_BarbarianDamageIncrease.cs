248cc2dd64772a21cfc6f6853e220d3c5e93f9c6
SHAW101298 <shaw101298@gmail.com>
2023-01-28 20:26:50 +0100

    barbarian czerwony na szale

--- Assets/Scripts/Player/Abilities/Player_BarbarianDamageIncrease.cs
+++ Assets/Scripts/Player/Abilities/Player_BarbarianDamageIncrease.cs
@@ -6,8 +6,16 @@ public class Player_BarbarianDamageIncrease : Ability_2
 {
     public float damageMultiplier = 4;
     public float basePlayerDamage;
-    public float durationTime;
+    public float durationTime = 5;
     public bool isIncreased;
+
+    public Color bodyColor = new Color(1f, 0.65f, 0.65f);
+    public Color weaponColor = new Color(0.85f, 0.25f, 0.25f);
+
+    SpriteRenderer bodySprite;
+    SpriteRenderer weaponSprite;
+
+
     public override void Use()
     {
         if(player.stats.currentEnergy >= energyCost && isRecharching == false)
@@ -15,6 +23,21 @@ public class Player_BarbarianDamageIncrease : Ability_2
             player.stats.ModifyEnergy(-energyCost);
             IncreaseDamage();
             player.ui_updater.Ability2Used();
+            //player.controller.animBody.SetTrigger("damageIncrease");
+            if(bodySprite == null)
+            {
+                foreach(Transform child in player.transform)
+                {
+                    if (child.name == "Player Body")
+                    {
+                        bodySprite = child.GetComponent<SpriteRenderer>();
+                        
+                    }
+                }
+                weaponSprite = player.controller.weaponCollider.gameObject.GetComponent<SpriteRenderer>();
+            }
+            bodySprite.color = bodyColor;
+            weaponSprite.color = weaponColor;
         }
     }
 
@@ -41,6 +64,8 @@ public class Player_BarbarianDamageIncrease : Ability_2
                 isRecharching = true;
                 player.stats.damage = basePlayerDamage;
                 isIncreased = false;
+                bodySprite.color = Color.white;
+                weaponSprite.color = Color.white;
             }
         }
         if(isRecharching == true)
@@ -51,6 +76,7 @@ public class Player_BarbarianDamageIncrease : Ability_2
                 timer = 0;
                 isRecharching = false;
                 player.ui_updater.Ability2Recharged();
+                
             }
         }
     }
